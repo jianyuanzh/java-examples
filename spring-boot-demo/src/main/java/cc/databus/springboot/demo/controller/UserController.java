1ea7e47b0b1c1a69e272bc549348df4baa439b45
Jianyuan <vincent.zhang@logicmonitor.com>
2018-08-27 22:33:48 +0800

    spring boot and redis

--- spring-boot-demo/src/main/java/cc/databus/springboot/demo/controller/UserController.java
+++ spring-boot-demo/src/main/java/cc/databus/springboot/demo/controller/UserController.java
@@ -4,7 +4,9 @@ import cc.databus.springboot.demo.pojo.JsonResponse;
 import cc.databus.springboot.demo.pojo.SysUser;
 import cc.databus.springboot.demo.pojo.User;
 import cc.databus.springboot.demo.service.UserService;
+import cc.databus.springboot.demo.utils.JsonUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +20,9 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
 
     @RequestMapping("/user")
     public JsonResponse getUser() {
@@ -65,5 +70,30 @@ public class UserController {
     @RequestMapping("/customquery/{id}")
     public JsonResponse customQuery(@PathVariable String id) {
         return new JsonResponse(200, "OK", userService.queryUserByIdCustom(id));
+    }
+
+
+    @RequestMapping("/redis/string")
+    public JsonResponse saveToRedis() {
+        redisTemplate.opsForValue().set("imooc-cache", "Hello boy!");
+        return new JsonResponse(200, "OK", redisTemplate.opsForValue().get("imooc-cache"));
+    }
+
+    @RequestMapping("/redis/user")
+    public JsonResponse redisUser() {
+
+        SysUser sysUser = new SysUser();
+        sysUser.setId("" + System.nanoTime());
+        sysUser.setNickname("lee" + System.currentTimeMillis() );
+        sysUser.setUsername("lee" + System.currentTimeMillis());
+        sysUser.setPassword("dfagda");
+        sysUser.setAge(18);
+        sysUser.setFaceImage("user/dfdg/");
+        sysUser.setJob(1);
+        sysUser.setSex(2);
+
+
+        redisTemplate.opsForValue().set("user:lee", JsonUtils.objectToJson(sysUser));
+        return new JsonResponse(200, "OK", JsonUtils.jsonToPojo(redisTemplate.opsForValue().get("user:lee"), SysUser.class));
     }
 }
